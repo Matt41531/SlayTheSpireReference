40bf779a36ad14bbdb6572bbe3d8a6326789fa0d
Matthew Rife <dxfan218@yahoo.com>
2025-02-17 12:24:50 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold hover:text-blue-300">
+        React + Vite + Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
